f8eb1aea5c4c410922a23be90f1225ffe8ce1012
marwan.mohamed <marwan.mohamed@z2data.com>
2025-03-15 17:07:27 +0200

    SQL

---
+++ SQL/HumanCapitalDb/HumanCapitalDb/dbo/Tables/Company.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Company] (
+    [Id]      UNIQUEIDENTIFIER NOT NULL,
+    [Name]    NVARCHAR (60)    NOT NULL,
+    [Address] NVARCHAR (60)    NOT NULL,
+    CONSTRAINT [PK_Company] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ SQL/HumanCapitalDb/HumanCapitalDb/dbo/Tables/Employee.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]        UNIQUEIDENTIFIER NOT NULL,
+    [Name]      NVARCHAR (MAX)   NOT NULL,
+    [Age]       INT              NOT NULL,
+    [CompanyId] UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Employee_Company_CompanyId] FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Company] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Employee_CompanyId]
+    ON [dbo].[Employee]([CompanyId] ASC);
+
